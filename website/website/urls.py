c5b8138c55e61b4630c6d5f79999ce1a96186a34
volodyabravo <sobolev.main@gmail.com>
2021-10-09 21:39:20 +0300

    Add fix

--- website/website/urls.py
+++ website/website/urls.py
@@ -29,6 +29,6 @@ router.register(r'orders', MapOrderView)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(router.urls)),
+    path('api/', include(router.urls)),
     path('api-auth/', include('rest_framework.urls'))
 ]
