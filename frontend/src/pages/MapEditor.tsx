e137a7cd60c6e96ab52838f2bd663655f8b3fa40
Alex Uskov <username@users.noreply.github.com>
2021-10-10 19:12:12 +0300

    Add size changes

--- frontend/src/pages/MapEditor.tsx
+++ frontend/src/pages/MapEditor.tsx
@@ -19,13 +19,15 @@ const CardArea = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
-    padding-top: 2em;
+    /* padding-top: 2em; */
+    height: calc(100vh - 64px);
 `;
 
 const CardContainer = styled.div`
     position: relative;
     width: 590px;
     height: 855px;
+    transform: scale(0.75);
     box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
 `;
 const CardTextContainer = styled.div`
