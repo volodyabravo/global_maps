c1219bd313638402fb06e527025696b65755b3d5
volodyabravo <sobolev.main@gmail.com>
2021-10-10 21:54:38 +0300

    Add admin fixes

--- website/catalog/models.py
+++ website/catalog/models.py
@@ -13,6 +13,13 @@ class MapTheme(models.Model):
     preview = models.ImageField(_('Preview image'), upload_to='uploads/images/preview/', blank=True)
     data = models.JSONField(_('JSON data'), blank=True, null=True)
 
+    def __str__(self):
+        return '{0} для {1}'.format(self.name, MapTypes.TYPES.get(self.product))
+
+    class Meta:
+        verbose_name = 'Тема'
+        verbose_name_plural = 'Темы'
+
 
 class MapSize(models.Model):
     active = models.BooleanField(_('Is this active'), default=True)
@@ -26,11 +33,25 @@ class MapSize(models.Model):
     units = models.IntegerField(_('Map size units'), default=1, blank=False, null=False,
                                 choices=[(key, value) for key, value in MapSizeUnits.UNITS.items()])
 
+    def __str__(self):
+        return '{0}'.format(self.name)
+
+    class Meta:
+        verbose_name = 'Размер'
+        verbose_name_plural = 'Размеры'
+
 
 class MapPrices(models.Model):
     price = MoneyField(_('Price'), max_digits=10, decimal_places=2, null=True, default_currency='RUB')
     product = models.IntegerField(_('Map'), choices=[(key, value) for key, value in MapTypes.TYPES.items()], default=0,
                                   blank=False, null=False, unique=True)
+
+    def __str__(self):
+        return '{0}'.format(MapTypes.TYPES.get(self.product))
+
+    class Meta:
+        verbose_name = 'Цена'
+        verbose_name_plural = 'Цены'
 
 
 class MapVersions(models.Model):
@@ -42,6 +63,13 @@ class MapVersions(models.Model):
     name = models.CharField(_('Name'), blank=False, null=False, max_length=500)
     image = models.ImageField(_('Image'), upload_to='uploads/images/static/', blank=True)
 
+    def __str__(self):
+        return '{0}'.format(self.name)
+
+    class Meta:
+        verbose_name = 'Версия'
+        verbose_name_plural = 'Версии'
+
 
 class MapOrder(models.Model):
     status = models.IntegerField(_('Status'), default=1, blank=False, null=False,
@@ -49,3 +77,10 @@ class MapOrder(models.Model):
     date = models.DateTimeField(_('Date and time'), auto_now=True)
     data = models.JSONField(_('JSON data'), blank=True, null=True)
     image = models.ImageField(_('Generated image'), upload_to='uploads/images/generated/', blank=True)
+
+    def __str__(self):
+        return '{0} {1}'.format(MapOrderStatuses.STATUSES.get(self.status), self.date)
+
+    class Meta:
+        verbose_name = 'Заказ'
+        verbose_name_plural = 'Заказы'
