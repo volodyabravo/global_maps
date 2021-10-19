b3d064cd57acbad950c1f519d3c192529211c17d
volodyabravo <sobolev.main@gmail.com>
2021-10-20 01:23:52 +0300

    Add admin filters 1

--- website/catalog/admin.py
+++ website/catalog/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from .models import MapTheme, MapSize, MapPrices, MapVersions, Order, MapOrder
 
-admin.site.register(MapTheme)
+
+class MapThemeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'active', 'order', 'product')
+    list_filter = ['active', 'product']
+    search_fields = ['name', 'product']
+
+
+admin.site.register(MapTheme, MapThemeAdmin)
 admin.site.register(MapSize)
 admin.site.register(MapPrices)
 admin.site.register(MapVersions)
