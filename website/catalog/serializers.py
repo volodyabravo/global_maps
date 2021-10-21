443c3eeba1fa25e2514d5a31b2a6183277028e73
volodyabravo <sobolev.main@gmail.com>
2021-10-21 04:41:56 +0300

    Add id to serializer

--- website/catalog/serializers.py
+++ website/catalog/serializers.py
@@ -3,15 +3,19 @@ from .models import MapTheme, MapSize, MapPrices, MapVersions, Order
 
 
 class MapThemeSerializer(serializers.HyperlinkedModelSerializer):
+    id = serializers.ReadOnlyField()
+
     class Meta:
         model = MapTheme
-        fields = ['product', 'name', 'preview', 'data']
+        fields = ['product', 'name', 'preview', 'data', 'id']
 
 
 class MapSizeSerializer(serializers.HyperlinkedModelSerializer):
+    id = serializers.ReadOnlyField()
+
     class Meta:
         model = MapSize
-        fields = ['version', 'price', 'name', 'height', 'width']
+        fields = ['version', 'price', 'name', 'height', 'width', 'id']
 
 
 class MapPricesSerializer(serializers.HyperlinkedModelSerializer):
@@ -21,9 +25,11 @@ class MapPricesSerializer(serializers.HyperlinkedModelSerializer):
 
 
 class MapVersionsSerializer(serializers.HyperlinkedModelSerializer):
+    id = serializers.ReadOnlyField()
+
     class Meta:
         model = MapVersions
-        fields = ['version', 'price', 'name', 'image']
+        fields = ['version', 'price', 'name', 'image', 'id']
 
 
 class OrderSerializer(serializers.HyperlinkedModelSerializer):
