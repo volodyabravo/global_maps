190998001885f6d8399f7ef0cd30d8b4038a361b
Alex Uskov <frozenalex@users.noreply.github.com>
2021-10-24 18:00:29 +0300

    Add static serving in debug

--- website/website/urls.py
+++ website/website/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
+from django.conf import settings
+from django.conf.urls.static import static
 from rest_framework import routers
 from catalog.views import MapThemeView, MapSizeView, MapPricesView, MapVersionsView, MapOrderView
 
@@ -32,3 +34,6 @@ urlpatterns = [
     path('api/', include(router.urls)),
     path('api-auth/', include('rest_framework.urls'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
