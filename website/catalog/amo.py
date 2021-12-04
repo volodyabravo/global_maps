7852a1d8429dbc982a3f416e9949eb3db46f07fc
volodyabravo <sobolev.main@gmail.com>
2021-12-04 05:09:57 +0300

    Add logging to amo

--- website/catalog/amo.py
+++ website/catalog/amo.py
@@ -3,7 +3,10 @@ from amocrm.v2 import Company as _Company
 from amocrm.v2 import Lead as _Lead
 from amocrm.v2 import custom_field
 from amocrm.v2 import tokens
+import logging
 import os
+
+order_logger = logging.getLogger('order')
 
 AMO_CLIENT_ID = os.getenv('AMO_CLIENT_ID', '0')
 AMO_CLIENT_SECRET = os.getenv('AMO_CLIENT_SECRET', '0')
@@ -32,55 +35,61 @@ class Contact(_Contact):
 
 
 def send_order_to_ammo(instance):
-    if AMO_CLIENT_ID:
-        t = tokens.default_token_manager(
-            client_id=AMO_CLIENT_ID,
-            client_secret=AMO_CLIENT_SECRET,
-            subdomain=AMO_SUBDOMAIN,
-            redirect_url=AMO_REDIRECT_URL,
-            storage=tokens.FileTokensStorage(DIR_NAME),  # by default FileTokensStorage
-        )
-        tokens.default_token_manager.init(code=AMO_REFRESH_TOKEN, skip_error=True)
+    try:
+        if AMO_CLIENT_ID:
+            t = tokens.default_token_manager(
+                client_id=AMO_CLIENT_ID,
+                client_secret=AMO_CLIENT_SECRET,
+                subdomain=AMO_SUBDOMAIN,
+                redirect_url=AMO_REDIRECT_URL,
+                storage=tokens.FileTokensStorage(DIR_NAME),  # by default FileTokensStorage
+            )
+            tokens.default_token_manager.init(code=AMO_REFRESH_TOKEN, skip_error=True)
 
-        lead = Lead(name=instance.name,
-                    status=instance.status,
-                    delivery_type=instance.delivery_type_name,
-                    address=instance.delivery_address,
-                    comments=instance.comment,
-                    order_id=instance.id,
-                    order_link="https://stylemaps.ru/admin/catalog/order/{0}/change/".format(instance.id),
-                    )
-        lead.create()
-        instance.ammo_id = lead.id
+            lead = Lead(name=instance.name,
+                        status=instance.status,
+                        delivery_type=instance.delivery_type_name,
+                        address=instance.delivery_address,
+                        comments=instance.comment,
+                        order_id=instance.id,
+                        order_link="https://stylemaps.ru/admin/catalog/order/{0}/change/".format(instance.id),
+                        )
+            lead.create()
+            instance.ammo_id = lead.id
 
-        contact = Contact(first_name=instance.name,
-                          last_name=instance.surname,
-                          phone=instance.phone,
-                          email=instance.email,
-                          country=instance.delivery_region,
-                          city=instance.delivery_city_name,
-                          )
-        contact.create()
+            contact = Contact(first_name=instance.name,
+                              last_name=instance.surname,
+                              phone=instance.phone,
+                              email=instance.email,
+                              country=instance.delivery_region,
+                              city=instance.delivery_city_name,
+                              )
+            contact.create()
 
-        lead.contacts.append(contact)
-        lead.save()
+            lead.contacts.append(contact)
+            lead.save()
+    except Exception as e:
+        order_logger.error('Failed to send order to amo: "%s"' % e)
 
 
 def sync_orders(instance):
-    if AMO_CLIENT_ID:
-        tokens.default_token_manager(
-            client_id=AMO_CLIENT_ID,
-            client_secret=AMO_CLIENT_SECRET,
-            subdomain=AMO_SUBDOMAIN,
-            redirect_url=AMO_REDIRECT_URL,
-            storage=tokens.FileTokensStorage(DIR_NAME),
-            # products=instance.products,
-            # images=instance.images,  # by default FileTokensStorage
-        )
-        tokens.default_token_manager.init(code=AMO_REFRESH_TOKEN, skip_error=True)
+    try:
+        if AMO_CLIENT_ID:
+            tokens.default_token_manager(
+                client_id=AMO_CLIENT_ID,
+                client_secret=AMO_CLIENT_SECRET,
+                subdomain=AMO_SUBDOMAIN,
+                redirect_url=AMO_REDIRECT_URL,
+                storage=tokens.FileTokensStorage(DIR_NAME),
+                # products=instance.products,
+                # images=instance.images,  # by default FileTokensStorage
+            )
+            tokens.default_token_manager.init(code=AMO_REFRESH_TOKEN, skip_error=True)
 
-        lead = Lead.objects.get(instance.ammo_id)
-        lead.status = instance.status
-        lead.order_id = instance.id
-        lead.order_link = "https://stylemaps.ru/admin/catalog/order/{0}/change/".format(instance.id)
-        lead.save()
+            lead = Lead.objects.get(instance.ammo_id)
+            lead.status = instance.status
+            lead.order_id = instance.id
+            lead.order_link = "https://stylemaps.ru/admin/catalog/order/{0}/change/".format(instance.id)
+            lead.save()
+    except Exception as e:
+        order_logger.error('Failed to update order at amo: "%s"' % e)
