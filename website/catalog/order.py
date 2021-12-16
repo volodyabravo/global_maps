96b49d1aaf8fcf08eb90fd82008ab38af049fa2b
volodyabravo <sobolev.main@gmail.com>
2021-12-16 05:59:02 +0300

    WIP API FIX

--- website/catalog/order.py
+++ website/catalog/order.py
@@ -16,80 +16,80 @@ order_logger = logging.getLogger('order')
 @csrf_exempt
 def order_create(request):
     if request.method == 'POST':
-        try:
-            data = json.loads(request.body)
-            order_logger.info('New order: "%s"' % data)
-            delivery = data.get('delivery')
-            personal = data.get('personal')
-            products = data.get('products')
-            delivery_price_response = get_delivery_methods_by_city(request)
-            delivery_price_json = json.loads(delivery_price_response.content)
-            delivery_price = 0
-            if delivery_price_json:
-                delivery_price = delivery_price_json.get(delivery.get('delivery_type_name')).get('delivery_price')
-            delivery_address = delivery.get('delivery_street')
-            if delivery.get('delivery_entrance'):
-                delivery_address += ', подтезд {0}'.format(delivery.get('delivery_entrance'))
-            if delivery.get('delivery_floor'):
-                delivery_address += ', этаж {0}'.format(delivery.get('delivery_floor'))
-            if delivery.get('delivery_apartments'):
-                delivery_address += ', квартира {0}'.format(delivery.get('delivery_apartments'))
-            order = Order.objects.create(
-                name=personal.get('name'),
-                email=personal.get('email'),
-                phone=personal.get('phone'),
-                comment=personal.get('comment'),
-                call_back=personal.get('call_back'),
-                emails_agree=personal.get('emails_agree'),
-                delivery_type_name=delivery.get('delivery_type_name'),
-                delivery_type_id=delivery.get('delivery_type_id'),
-                delivery_city_name=delivery.get('delivery_city_name'),
-                delivery_region=delivery.get('delivery_region'),
-                delivery_city_id=delivery.get('delivery_city_id'),
-                delivery_address=delivery_address,
-                pvz_id=delivery.get('pvz_id'),
-                card_data=data,
-                delivery_price=delivery_price
-            )
+        # try:
+        data = json.loads(request.body)
+        order_logger.info('New order: "%s"' % data)
+        delivery = data.get('delivery')
+        personal = data.get('personal')
+        products = data.get('products')
+        delivery_price_response = get_delivery_methods_by_city(request)
+        delivery_price_json = json.loads(delivery_price_response.content)
+        delivery_price = 0
+        if delivery_price_json:
+            delivery_price = delivery_price_json.get(delivery.get('delivery_type_name')).get('delivery_price')
+        delivery_address = delivery.get('delivery_street')
+        if delivery.get('delivery_entrance'):
+            delivery_address += ', подтезд {0}'.format(delivery.get('delivery_entrance'))
+        if delivery.get('delivery_floor'):
+            delivery_address += ', этаж {0}'.format(delivery.get('delivery_floor'))
+        if delivery.get('delivery_apartments'):
+            delivery_address += ', квартира {0}'.format(delivery.get('delivery_apartments'))
+        order = Order.objects.create(
+            name=personal.get('name'),
+            email=personal.get('email'),
+            phone=personal.get('phone'),
+            comment=personal.get('comment'),
+            call_back=personal.get('call_back'),
+            emails_agree=personal.get('emails_agree'),
+            delivery_type_name=delivery.get('delivery_type_name'),
+            delivery_type_id=delivery.get('delivery_type_id'),
+            delivery_city_name=delivery.get('delivery_city_name'),
+            delivery_region=delivery.get('delivery_region'),
+            delivery_city_id=delivery.get('delivery_city_id'),
+            delivery_address=delivery_address,
+            pvz_id=delivery.get('pvz_id'),
+            card_data=data,
+            delivery_price=delivery_price
+        )
 
-            for product in products:
-                product_data = product.get('product_customization')
-                size = MapSize.objects.get(id=product_data.get('sizeId'))
-                version = MapVersions.objects.get(id=product_data.get('version')[-1])
-                price = MapPrices.objects.get(size=size, version=version)
-                theme = MapTheme.objects.get(id=product_data.get('theme'))
-                try:
-                    vector_image = VectorImages.objects.get(id=product_data.get('vector').get('image_id'))
-                    vector_color = VectorColors.objects.get(id=product_data.get('vector').get('color_id'))
-                except (VectorImages.DoesNotExist, VectorColors.DoesNotExist, AttributeError):
-                    vector_color = None
-                    vector_image = None
-                MapOrder.objects.create(
-                    order=order,
-                    size=size,
-                    theme=theme,
-                    product=theme.product,
-                    version=version,
-                    headline=product_data.get('headline'),
-                    divider=product_data.get('divider'),
-                    tagline=product_data.get('tagline'),
-                    subline=product_data.get('subline'),
-                    orientation=product_data.get('orientation'),
-                    lng=product_data.get('location', {}).get('lng', None),
-                    lat=product_data.get('location', {}).get('lat', None),
-                    city_name=product_data.get('location', {}).get('cityName', None),
-                    date=product_data.get('date'),
-                    zoom=product_data.get('zoom'),
-                    vector_color=vector_color,
-                    vector_image=vector_image,
-                    price=price.price
-                )
-            order.save()
-        except Exception as e:
-            print(e)
-            order_logger.error('Failed to create order: "%s"' % e)
-            order_logger.error('Probably missed order data: "%s"' % request.body)
-            return HttpResponse(status=400)
+        for product in products:
+            product_data = product.get('product_customization')
+            size = MapSize.objects.get(id=product_data.get('sizeId'))
+            version = MapVersions.objects.get(id=product_data.get('version')[-1])
+            price = MapPrices.objects.get(size=size, version=version)
+            theme = MapTheme.objects.get(id=product_data.get('theme'))
+            try:
+                vector_image = VectorImages.objects.get(id=product_data.get('vector').get('image_id'))
+                vector_color = VectorColors.objects.get(id=product_data.get('vector').get('color_id'))
+            except (VectorImages.DoesNotExist, VectorColors.DoesNotExist, AttributeError):
+                vector_color = None
+                vector_image = None
+            MapOrder.objects.create(
+                order=order,
+                size=size,
+                theme=theme,
+                product=theme.product,
+                version=version,
+                headline=product_data.get('headline'),
+                divider=product_data.get('divider'),
+                tagline=product_data.get('tagline'),
+                subline=product_data.get('subline'),
+                orientation=product_data.get('orientation'),
+                lng=product_data.get('location', {}).get('lng', None),
+                lat=product_data.get('location', {}).get('lat', None),
+                city_name=product_data.get('location', {}).get('cityName', None),
+                date=product_data.get('date'),
+                zoom=product_data.get('zoom'),
+                vector_color=vector_color,
+                vector_image=vector_image,
+                price=price.price
+            )
+        order.save()
+        # except Exception as e:
+        #     print(e)
+        #     order_logger.error('Failed to create order: "%s"' % e)
+        #     order_logger.error('Probably missed order data: "%s"' % request.body)
+        #     return HttpResponse(status=400)
         return JsonResponse({"order_id": order.id})
     return JsonResponse({"error": "Use POST"})
 
