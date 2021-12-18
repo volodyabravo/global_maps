be3e4572285d9a81251993d30ee0bd3b2ad12e40
volodyabravo <sobolev.main@gmail.com>
2021-12-19 02:55:26 +0300

    Add fix on printer admin

--- website/catalog/printer.py
+++ website/catalog/printer.py
@@ -1,14 +1,15 @@
-from .models import MapOrder, Order
 from django.core.files.images import ImageFile
-import re, io
-from base64 import decodebytes
-from django.core.files import File
+import io
+import os
 import requests
 
 
 def print_map(obj):
     json = obj.full_json
-    r = requests.post('http://puppet:6969/generatesync/', json=json)
+    host = os.getenv('PUPPET_HOST', 'puppet')
+    port = os.getenv('PUPPET_PORT', '6969')
+    path = 'http://{0}:{1}/generatesync/'.format(host, port)
+    r = requests.post(path, json=json)
 
     image = ImageFile(io.BytesIO(r.content), name="{0}_{1}.png".format(obj.pk, obj.date_created))
 
